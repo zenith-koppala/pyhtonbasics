3c00e38bb103d6519f99b617840f06ccf4e9f785
zenith-koppala <zenithkoppala@gmail.com>
2026-03-16 21:52:33 +0530

    Add files via upload

---
+++ PyIF.py
@@ -0,0 +1,3 @@
+age = 18
+if age >= 18:
+    print("You can vote")
---
+++ PyIFELIF.py
@@ -0,0 +1,10 @@
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 75:
+    print("Grade B")
+elif marks >= 50:
+    print("Grade C")
+else:
+    print("Fail")
