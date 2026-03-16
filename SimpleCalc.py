60f0baa7477f121a66ee279773052dbc796695b9
zenith-koppala <zenithkoppala@gmail.com>
2026-03-16 21:48:13 +0530

    Add files via upload

---
+++ SimpleCalc.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Division:", num1 / num2)
